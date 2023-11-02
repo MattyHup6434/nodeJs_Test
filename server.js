1691d11f42fd01975248fe3e32d2ae557bf9ef98
MattyHup6434 <beembeem6434@gmail.com>
2023-11-02 10:05:28 +0700

    test_1_add_all
    server()

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const app = express();
+const port = 4500;
+
+app.use(morgan('combined'))
+
+
+app.get('/',(req,res) =>{
+    res.send('hello World');
+});
+
+app.listen(port, ()=>{
+    console.log('server is runing on port :',port);
+});
